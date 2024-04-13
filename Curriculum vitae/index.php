ab086f401610202f730620816c02be06f9877e76
Bouelkheir Yassine <31709973+yassinebouelkheir@users.noreply.github.com>
2024-04-13 17:58:40 +0200

    Create index.php

---
+++ Curriculum vitae/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Curriculum vitae - BOUELKHEIR Yassine</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"/>
+</head>
+<body>
+
+</body>
+</html>

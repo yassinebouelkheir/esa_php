19840724d86056f4190fdaec00bf667b0c1843d9
Bouelkheir Yassine <31709973+yassinebouelkheir@users.noreply.github.com>
2024-04-15 01:07:24 +0200

    Upload CV v1.0

--- Curriculum vitae/index.php
+++ Curriculum vitae/index.php
@@ -1,12 +1,70 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Curriculum vitae - BOUELKHEIR Yassine</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"/>
-</head>
-<body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Curriculum vitae - BOUELKHEIR Yassine</title>
+		<link rel="stylesheet" href="css/style.css"/>
+	</head>
+	<body>
+    <div class="grid-container">
+    	<div class="item1">
+    		<img src="images/profile.jpeg" width="230px" style="border-radius: 50%;">
+    		<h2>Yassine BOUELKHEIR</h2>
+    		<h5>Etudiant à l'Ecole Supérieure des Affaires</h5>
+    	</div>
+    	<div class="item2">
+    		<h4 style="padding-top: 60px;">Address:</h4>
+    		<p>Rue des ursulines 1/0040<br>5000 Namur</p>
+    		<h4>Tel:</h4>
+    		<p>+32 489 32 63 81</p>
+    	    <h4>Email:</h4>
+    		<p>yassinebouelkheir@gmail.com</p>
+    		<h4>Langues:</h4>
+    		<p>Français<br>Anglais<br>Arabe</p>
+    	    <h4>Certificats:</h4>
+    		<p><strong>EFSET Certificate</strong><br>EF Standard English Test<br>C1 Advanced<br>Aug 2021</p>
+    		<p><strong>Diplôme en programmation C</strong><br>Alison<br>Dec 2019</p>
+    		<h4>Personalité:</h4>
+    		<p>Créativité<br>Sérieux<br>Flexible<br>Organisé</p>
+    	</div>
+    	<div class="item3">
+    		<div class="mainbody">
+    			<h2>EDUCATION</h2>
+    			<h4>2023 - Présent</h4>
+    			<h4>Ecole supérieure des affaires</h4>
+    			<p>Bachelier en Sciences informatique</p>
+    		    <h4>2022 - 2023</h4>
+    			<h4>Université de Namur</h4>
+    			<p>Bachelier en Sciences informatique</p>
+    		    <h4>2019 - 2022</h4>
+    			<h4>Université Mohammed V de Rabat</h4>
+    			<p>Diplôme universitaire de technologie en Génie électrique et informatique industrielle</p>
+    			<h4>2016 - 2019</h4>
+    			<h4>Lycée les oranges de Rabat</h4>
+    			<p>Baccalauréat en Sciences et technologies électrique</p>
 
-</body>
+    			<h2 style="margin-top: 100px;">EXPERIENCE PROFESSIONNELLE</h2>
+    			<h4>Juillet 2023 - Présent</h4>
+    			<h4>Job étudiant - Ouvrier/Livreur</h4>
+    			<p>COUSCOUS GOURMAND SRL</p>
+    		    <h4>Novembre 2022 - Présent</h4>
+    			<h4>Job étudiant - Livreur</h4>
+    			<p>UBEREATS</p>
+    		   	<h4>Decembre 2022 - Avril 2023</h4>
+    			<h4>Job étudiant - Ouvrier/Livreur</h4>
+    			<p>PIZZAHUT NAMUR</p>
+    		   	<h4>Avril 2022 - Juin 2022</h4>
+    			<h4>Stage - Technicien Electrique</h4>
+    			<p>GROUP RENELEC<br><br>Installation et maintenance de différents<br>équipements électriques et domotiques et conception de câblage de grandes surfaces.</p>
+    		    <h4>Avril 2021 - Aoùt 2021</h4>
+    			<h4>Stage - Technicien Informatique</h4>
+    			<p>IRITEL S.A.R.L<br><br>Installation et configuration de différents matériels<br>informatiques tels que serveurs, routeurs, caméras ip, téléphones ip.</p>
+    		    <h4>Juin 2020</h4>
+    			<h4>Stage - Stage d'initiation</h4>
+    			<p>Ecole Supérieure de Technologie<br><br>Création et réalisation d'un système d'arrosage<br> automatique IoT en utilisant des composants de courant faible. (Arduino, Module WIFI, ...)</p>
+    		</div>
+    	</div>  
+    </div>
+	</body>
 </html>
